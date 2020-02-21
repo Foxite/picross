e6aa8a2333c3e97ad1688e372cce7d0685188145
foxite <d.kok.2000@gmail.com>
2020-02-21 21:34:50 +0100

    typo

--- util.js
+++ util.js
@@ -3,7 +3,7 @@ function CircularArray(array) {
 	this.length = this._array.length;
 	this.contains = this._array.contains;
 	this.get = function(index) {
-		return this._array[((index % this.length) + this.length) % this.length]; // // https://stackoverflow.com/a/4467559
+		return this._array[((index % this.length) + this.length) % this.length]; // https://stackoverflow.com/a/4467559
 	}
 	this.set = function(index, item) {
 		this._array[((index % this.length) + this.length) % this.length] = item;
