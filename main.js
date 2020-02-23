842655e87fb3ffa85426f302684034a416e4df51
foxite <d.kok.2000@gmail.com>
2020-02-23 14:40:06 +0100

    merged the validate() for loops

--- main.js
+++ main.js
@@ -32,94 +32,60 @@ function rotateCellContent(element, direction) {
 }
 
 function validate() {
+	validateStep = function(forFunc, hintCell, i) {
+		hintCell.classList.remove("incorrect");
+		Array.from(hintCell.getElementsByClassName("number")).forEach(function(number) {
+			number.classList.remove("crossed");
+		});
+		let spec = hintCell.textContent.split(" ").map(function(item) {
+			return parseInt(item);
+		});
+		let specI = 0;
+		let inSequence = false;
+		forFunc(i, function(cell) {
+			if (cell.classList.contains("filled")) {
+				if (specI >= spec.length) {
+					hintCell.classList.add("incorrect");
+				} else {
+					inSequence = true;
+					spec[specI] = spec[specI] - 1;
+					if (spec[specI] < 0) {
+						hintCell.classList.add("incorrect");
+					}
+				}
+			} else if (inSequence) {
+				if (spec[specI] == 0) {
+					hintCell.getElementsByClassName("number")[specI].classList.add("crossed");
+				} else if (spec[specI] < 0) {
+					hintCell.classList.add("incorrect");
+				}
+				specI++;
+				inSequence = false;
+			}
+		});
+		if (hintCell.classList.contains("incorrect")) {
+			// if it's incorrect, uncross all crossed hints
+			Array.from(hintCell.getElementsByClassName("number")).forEach(function(number) {
+				number.classList.remove("crossed");
+			});
+		} else if (spec[spec.length - 1] == 0) {
+			// cross off the last hint, it won't get crossed by the code in the loop
+			hintCell.getElementsByClassName("number")[spec.length - 1].classList.add("crossed");
+		}
+	};
+
 	Array.from(puzzle.getElementsByClassName("crossed")).map(function(item) {
 		item.classList.remove("crossed");
 		return 0;
 	});
-	// TODO find a way to merge this and the next code block, preferably without it becoming a mess
+	
 	// Validate rows
 	for (let i = 0; i < puzzleHeight; i++) {
-		puzzle.rows[i + 1].cells[0].classList.remove("incorrect");
-		Array.from(puzzle.rows[i + 1].cells[0].getElementsByClassName("number")).forEach(function(number) {
-			number.classList.remove("crossed");
-		});
-		let spec = puzzle.rows[i + 1].cells[0].textContent.split(" ").map(function(item) {
-			return parseInt(item);
-		});
-		let specI = 0;
-		let inSequence = false;
-		forRow(i, function(cell) {
-			if (cell.classList.contains("filled")) {
-				if (specI >= spec.length) {
-					puzzle.rows[i + 1].cells[0].classList.add("incorrect");
-				} else {
-					inSequence = true;
-					spec[specI] = spec[specI] - 1;
-					if (spec[specI] < 0) {
-						puzzle.rows[i + 1].cells[0].classList.add("incorrect");
-					}
-				}
-			} else if (inSequence) {
-				if (spec[specI] == 0) {
-					puzzle.rows[i + 1].cells[0].getElementsByClassName("number")[specI].classList.add("crossed");
-				} else if (spec[specI] < 0) {
-					puzzle.rows[i + 1].cells[0].classList.add("incorrect");
-				}
-				specI++;
-				inSequence = false;
-			}
-		});
-		if (puzzle.rows[i + 1].cells[0].classList.contains("incorrect")) {
-			// if it's incorrect, uncross all crossed hints
-			Array.from(puzzle.rows[i + 1].cells[0].getElementsByClassName("number")).forEach(function(number) {
-				number.classList.remove("crossed");
-			});
-		} else if (spec[spec.length - 1] == 0) {
-			// cross off the last hint, it won't get crossed by the code in the loop
-			puzzle.rows[i + 1].cells[0].getElementsByClassName("number")[spec.length - 1].classList.add("crossed");
-		}
+		validateStep(forRow, puzzle.rows[i + 1].cells[0], i);
 	}
 	
 	// Validate columns
 	for (let i = 0; i < puzzleWidth; i++) {
-		puzzle.rows[0].cells[i + 1].classList.remove("incorrect");
-		Array.from(puzzle.rows[0].cells[i + 1].getElementsByClassName("number")).forEach(function(number) {
-			number.classList.remove("crossed");
-		});
-		let spec = puzzle.rows[0].cells[i + 1].textContent.split(" ").map(function(item) {
-			return parseInt(item);
-		});
-		let specI = 0;
-		let inSequence = false;
-		forColumn(i, function(cell) {
-			if (cell.classList.contains("filled")) {
-				if (specI >= spec.length) {
-					puzzle.rows[0].cells[i + 1].classList.add("incorrect");
-				} else {
-					inSequence = true;
-					spec[specI] = spec[specI] - 1;
-					if (spec[specI] < 0) {
-						puzzle.rows[0].cells[i + 1].classList.add("incorrect");
-					}
-				}
-			} else if (inSequence) {
-				if (spec[specI] == 0) {
-					puzzle.rows[0].cells[i + 1].getElementsByClassName("number")[specI].classList.add("crossed");
-				} else if (spec[specI] < 0) {
-					puzzle.rows[0].cells[i + 1].classList.add("incorrect");
-				}
-				specI++;
-				inSequence = false;
-			}
-		});
-		if (puzzle.rows[0].cells[i + 1].classList.contains("incorrect")) {
-			// if it's incorrect, uncross all crossed hints
-			Array.from(puzzle.rows[0].cells[i + 1].getElementsByClassName("number")).forEach(function(number) {
-				number.classList.remove("crossed");
-			});
-		} else if (spec[spec.length - 1] == 0) {
-			// cross off the last hint, it won't get crossed by the code in the loop
-			puzzle.rows[0].cells[i + 1].getElementsByClassName("number")[spec.length - 1].classList.add("crossed");
-		}
+		validateStep(forColumn, puzzle.rows[0].cells[i + 1], i);
 	}
 }
